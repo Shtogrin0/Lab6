0725e74b36bbec5b2374caca7ed2f2abc872562d
Shtogrin0 <Shtogrin0@bigmir.net>
2017-11-17 03:02:51 +0300

    Lab6Commit

---
+++ ui.R
@@ -0,0 +1,18 @@
+library(shiny)
+shinyUI(fluidPage(
+  titlePanel("Yahoo Finance Gold Futures"),
+  sidebarLayout(
+    sidebarPanel(
+      textInput("symb", "Symbol", "GOLD"),
+      dateRangeInput("dates",
+        "Date range",
+        start = "2017-01-27",
+        end = as.character(Sys.Date())),
+      checkboxInput("log", "Plot y axis on log scale",
+        value = FALSE),
+      checkboxInput("adjust",
+        "Adjust prices for inflation", value = FALSE)
+    ),
+    mainPanel(plotOutput("plot"))
+  )
+))
